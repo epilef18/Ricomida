69d6634eae15964ace9893d8b64ebae35bd421af
Fvsanles <f.villasanles@gmail.com>
2024-07-15 15:39:29 -0400

    segundo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,30 @@
-$(document).ready(function(){
-  $('#botoncorreo').on('click',function(){
-      alert('El correo fue enviado correctamente...')
-  });       
+$(document).ready(function () {
+  $("#botoncorreo").on("click", function () {
+    alert("El correo fue enviado correctamente...");
+  });
+  $("#ingredientes").dblclick(function () {
+    $("#ingredientes").toggleClass("negro rojo");
+  });
+  $("#preparacion").dblclick(function () {
+    $("#preparacion").toggleClass("negro rojo");
+  });
+  $("#panqueque").on("click", function () {
+    $("#panquequecont").toggle("fast", function () {});
+  });
+  $("#panqueque").on("click", function () {
+    $("#panquequeimg").toggle("fast", function () {});
+  });
+  $("#tiramisu").on("click", function () {
+    $("#tiramisuimg").toggle("fast", function () {});
+  });
+  $("#tiramisu").on("click", function () {
+    $("#tiramisucont").toggle("fast", function () {});
+  });
+  $("#plateada").on("click", function () {
+    $("#plateadaimg").toggle("fast", function () {});
+  });
+  $("#plateada").on("click", function () {
+    $("#plateadacont").toggle("fast", function () {});
+  });
+  $('[data-toggle="tooltip"]').tooltip();
 });
-
-$(document).ready(function(){
-  var divdbl = $( "ingredientes" ).first();
-  divdbl.on( "dblclick", function() {
-  divdbl.toggleClass( "dbl" );
-  } );     
-});
-
